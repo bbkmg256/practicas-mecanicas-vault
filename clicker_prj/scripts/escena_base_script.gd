61cec1360f1a1b89c1db9398b34e0fc2a0052ab2
bbkmg256 <tecded@proton.me>
2025-08-24 20:28:11 -0300

    primer commit

---
+++ clicker_prj/scripts/escena_base_script.gd
@@ -0,0 +1,70 @@
+# Script para nivel base de prueba
+
+# NOTE: HEREDA DEL NODO/CLASE Node2D
+extends Node2D
+
+###
+
+# VARIABLES
+# NOTE: PRECARGA LA ESCENA DEL MONSTRUO A MEMORIA
+var monstruo_escena : Object
+var ui_vida_escena : Object
+var instancia_mons : Node2D
+var instancia_ui_vida : Control
+var ubicacion_monstuo : Vector2
+var etiqueta_vida : Label
+#var player_anim_mons :  = null
+
+# METODOS
+func _ready() -> void:
+	self.monstruo_escena = preload("res://escenas/monstuo.tscn") # QUE PELOTUDO, ESCRIBÍ MAL EL NOMBRE DE LA ESCENA
+	self.ui_vida_escena = preload("res://escenas/ui_vida.tscn")
+	self.instancia_mons = null
+	self.instancia_ui_vida = self.ui_vida_escena.instantiate()
+	self.instancia_ui_vida.position = Vector2((200/2)+20, (100/2)-10)
+	self.etiqueta_vida = self.instancia_ui_vida.get_node("porcentaje_vida")
+	add_child(self.instancia_ui_vida)
+	self.ubicacion_monstuo = Vector2(200/2, 100/2)
+
+func _instanciar_entidad() -> void:
+	# NOTE: INSTANCIA DE LA ESCENA DEL MONS.
+	self.instancia_mons = self.monstruo_escena.instantiate()
+	self.instancia_mons.position = self.ubicacion_monstuo
+	# NOTE: AÑADE LA INSTANCIA AL ARBOL DE NODOS DE LA ESCENA
+	add_child(self.instancia_mons)
+	# NOTE: CONECTA A SEÑAL PERSONALIZADA "finalizado" DE LA ENTIDAD MONSTRUO
+	self.instancia_mons.connect(
+		"finalizado",
+		Callable(
+			self, "_on_anim_muerte_finalizada"
+	))
+	# NOTE: ESTA ES LA FORMA MODERNA DE ESRIBIR LO DE ARRIBA (GODOT 4.0)
+	# instancia_mons.finalizado.connect(_on_anim_muerte_finalizada)
+
+	#print(instancia_mons.get_vida_monstruo())
+
+func _librerar_entidad() -> void:
+	# NOTE: LIBERA LA INSTANCIA DE LA MEMORIA Y LIBERA LA REFERENCIA
+	self.instancia_mons.queue_free()
+	self.instancia_mons = null
+	#print("[LOG] > MAL")
+
+# BUG: ESTO SE EJECUTA INDEFINIDAMENTE (SOLUCINADO)
+func _on_anim_muerte_finalizada():
+	self._librerar_entidad()
+	#print("[LOG] > Funciona")
+
+# NOTE: SE LLAMA DURANTE TODA LA VIDA DE EJECUCION DEL JUEGO
+func _process(delta: float) -> void:
+	# BUG: "instancia_mons" siempre es null, por que no se esta
+	# referenciando al objeto directamente en la variable. (SOLUCIONADO)
+	if instancia_mons == null:
+		print("[LOG] > Un monstruo ah aparecido, destruyelo!")
+		self._instanciar_entidad()
+	# NOTE: ESTABLECE LA CANTIDAD DE VIDA DEL MOSNTRUO EN EL HUD
+	self.etiqueta_vida.text = str(self.instancia_mons.get_vida_monstruo())
+	# BUG: LA ANIMACIÓN DE MUERTE NO SE REPRODUCE (SOLUCINADO)
+	if self.instancia_mons != null and not self.instancia_mons.sigue_vivo():
+		self.instancia_mons.animacion_muerto()
+		#_librerar_entidad()
+	#print("[LOG] > Se ejecuta siempre")
---
+++ clicker_prj/scripts/monstuo_script.gd
@@ -0,0 +1,83 @@
+# NOTE: ESTE SCRIPT HEREDA DEL NODO/CLASE "Node2D"
+extends Node2D
+
+###
+
+# NOTE: DEFINICIÓN DE SEÑALES
+signal finalizado
+
+####
+
+# VARIABLES
+#var punto_golpe = 1
+var vida_monstruo : int
+# @onready var animation_player = $AnimationPlayer
+var animacion_monstruo : AnimationPlayer
+
+# GETTERS/SETTERS
+func get_vida_monstruo() -> int:
+	return self.vida_monstruo
+
+# METODOS
+# NOTE: SE EJECUTA UNA SOLA VEZ
+func _ready() -> void:
+	self.vida_monstruo = 50
+	self.animacion_monstruo = $AnimationPlayer
+	# NOTE: CONECTA A SEÑAL "animation_finished" de AnimationPlayer
+	self.animacion_monstruo.connect(
+		"animation_finished", 
+		Callable(
+			self, "_on_animation_finished"
+	))
+	# NOTE: ESTA ES LA FORMA MODERNA DE ESRIBIR LO DE ARRIBA (GODOT 4.0)
+	# animacion_monstruo.animation_finished.connect("_on_animation_finished")
+	
+	if not self.animacion_monstruo.is_playing():
+		self.animacion_monstruo.play("spawn_anim")
+	print("[LOG] Vida del monstruo -> " + str(self.vida_monstruo)) # LOG
+	#print("[LOG] > Funcional") # LOG
+
+func sigue_vivo() -> bool:
+	return self.vida_monstruo > 0
+
+func animacion_muerto() -> void:
+	if not self.animacion_monstruo.is_playing():
+		self.animacion_monstruo.play("ded_anim")
+		#print("[LOG] > funciona")
+	#print("[LOG] > Monstruo muerto!")
+
+# NOTE: METODO DE SEÑAL PERSONALIZADA PARA ANIMACIÓN
+func _on_animation_finished(nombre_anim: String) -> void:
+	if nombre_anim == "ded_anim":
+		emit_signal("finalizado")
+
+# NOTE: DETECTA EVENTOS SOBRE EL AREA2D DEL NODO, Y VERIFICA SI EL EVENTO ES UN CLICK
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	# NOTE: VERIFICA QUE EL EVENTO, SEA UN ENVENTO DEL MAOUSE
+	if event is InputEventMouseButton:
+		# NOTE: VERIFICA QUE EL EVENTO DEL MOUSE, SEA UN CLICK (IZQUIERDO EN ESTE CASO)
+		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+			# NOTE: EVITA QUE SE LEAN EVENTOS DEL CLICK MIENTRAS EL MONSTRUO ESTÁ MUERTO
+			if not self.sigue_vivo():
+				return
+				#print("[ LOG ] HAS MATADO AL MONSTRUO") # LOG
+			# REVIEW: LA ANIMACIÓN ES MUY LENTA
+			# (SOLUCIONADO: ANIMACIÓN REINICIADA CON CADA TOQUE)
+			if self.animacion_monstruo.is_playing() and self.animacion_monstruo.current_animation == "pop_anim":
+				self.animacion_monstruo.stop()
+				self.animacion_monstruo.seek(0.0, true)
+			self.animacion_monstruo.play("pop_anim")
+			self.vida_monstruo -= 1
+			print("[LOG] > Vida del monstruo -> " + str(self.vida_monstruo)) # LOG
+			# HACK: MALAZO, PERO DESPUES VEO QUE ONDA
+			if self.vida_monstruo <= 0:
+				print("[LOG] > Monstruo muerto!")
+
+func _process(delta: float) -> void:
+	# NOTE: ANIMACIÓN CONSTANTE EN IDLE
+	if not self.animacion_monstruo.is_playing():
+		self.animacion_monstruo.play("idle_anim")
+
+# DEPRECATE: CODIGO SIN USAR
+# if not animacion_monstruo.is_playing():
+	# animacion_monstruo.play("pop_anim")
